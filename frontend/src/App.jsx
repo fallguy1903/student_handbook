a6ef6341eafb044fc2e417730ec743d665a65b0e
fallguy <harishvijayan2003@gmail.com>
2025-03-30 12:16:16 +0530

    complete login frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import Login from './Login';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-        <h1>Hello Brothers</h1>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/Login.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { useNavigate } from "react-router-dom";
+import './App.css'
+
+function Login() {
+  const [regno, setRegno] = useState('');
+  const [password,setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleLogin = async (evt)=>{
+    evt.preventDefault();
+    console.log(JSON.stringify({regno,password}))
+    const response = await fetch('http://localhost:3000/login',{
+      method:"POST",
+      headers:{"Content-Type":"application/json"},
+      body:JSON.stringify({regno,password})
+    })
+    
+    const data = await response.json();
+    if(data.user == 'null')
+        {
+            console.log("Incorrect Credentials")
+            setRegno("");
+            setPassword("");
+            navigate("/");
+        }
+    else
+        {
+          console.log(data.user.username)
+          navigate("/home", { state: { user: data.user } } );
+        }
+    
+    
+  }
+  return (
+    <>
+        <h1>Login Page</h1>
+          <form onSubmit={handleLogin}>
+          <input type='text' name='regno' onChange={(e)=>setRegno(e.target.value)} value={regno} placeholder='regno'></input>
+          <input type='password' name='password' onChange={(e)=>setPassword(e.target.value)} value={password} placeholder='passowrd'></input>
+          <button type="submit">Login</button>
+        </form>
+        
+    </>
+  )
+}
+
+export default Login
